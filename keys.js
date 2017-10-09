325d3537a0516760f7f480b81e830ea48a287a13
Jose Arancibia <josearancibia89@gmail.com>
2017-10-09 17:54:52 -0400

    updating functionality.

---
+++ keys.js
@@ -0,0 +1,10 @@
+var twitterKeys = {
+  consumer_key: 'fvxN1jFtljR9tANRImwrLKeAp',
+  consumer_secret: 'DchDEBiPmUstNeGBgquHBh7G12T4cJ02TPY0yBiPsv91pFU6Qj',
+  access_token_key: '913236682304081922-9nTKTsmHd6slGY69BGH6rj49EmMPovl',
+  access_token_secret: '1LVOEJZ7bZRicUbqxQ9co5dIsunbJ9eTN5eZhc1gTKd6C',
+};
+
+
+module.exports = twitterKeys;
+
